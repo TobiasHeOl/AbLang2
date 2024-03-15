Fixed probability :bug: and length average :bug:

ablang2/models/ablang2/encoderblock.py
@@ -120,7 +120,7 @@ class MultiHeadAttention(torch.nn.Module):
         batch_size, seq_len, embed_dim = x.size()
         
         q, k, v = self.q_proj(x), self.k_proj(x), self.v_proj(x)
-        q *= self.scaling ## WHY DO WE DO THIS?????
+        q *= self.scaling
         
         q = q.contiguous().view(
             batch_size, 

ablang2/pretrained_utils/encodings.py
@@ -24,6 +24,27 @@ class AbEncoding:
         tokens = self.tokenizer(seqs, pad=True, w_extra_tkns=False, device=self.used_device)
         with torch.no_grad():
             return self.AbLang(tokens)
+        
+    def _predict_logits_with_step_masking(self, seqs):
+        
+        tokens = self.tokenizer(seqs, pad=True, w_extra_tkns=False, device=self.used_device)
+        
+        logits = []
+        for single_seq_tokens in tokens:
+            
+            tkn_len = len(single_seq_tokens)
+            masked_tokens = single_seq_tokens.repeat(tkn_len, 1)
+            for num in range(tkn_len):
+                masked_tokens[num, num] = self.tokenizer.mask_token
+            
+            with torch.no_grad():
+                logits_tmp = self.AbLang(masked_tokens)
+                       
+            logits_tmp = torch.stack([logits_tmp[num, num] for num in range(tkn_len)])
+
+            logits.append(logits_tmp)
+    
+        return torch.stack(logits, dim=0)        
 
     def seqcoding(self, seqs, **kwargs):
         """
@@ -34,9 +55,10 @@ class AbEncoding:
         
         lens = np.vectorize(len)(seqs)
         lens = np.tile(lens.reshape(-1,1,1), (encodings.shape[2], 1))
+
         return np.apply_along_axis(res_to_seq, 2, np.c_[np.swapaxes(encodings,1,2), lens])
         
-    def rescoding(self, seqs, align=False):
+    def rescoding(self, seqs, align=False, **kwargs):
         """
         Residue specific representations.
         """
@@ -46,24 +68,30 @@ class AbEncoding:
             
         else: return [res_to_list(state, seq) for state, seq in zip(encodings, seqs)]
         
-    def likelihood(self, seqs, align=False):
+    def likelihood(self, seqs, align=False, stepwise_masking=False, **kwargs):
         """
         Likelihood of mutations
         """
-        logits = self._predict_logits(seqs).cpu().numpy()
+        if stepwise_masking:
+            logits = self._predict_logits_with_step_masking(seqs).cpu().numpy()
+        else:
+            logits = self._predict_logits(seqs).cpu().numpy()
         
         if align: return logits
             
         else: return [res_to_list(state, seq) for state, seq in zip(logits, seqs)]
         
-    def probability(self, seqs, align=False):
+    def probability(self, seqs, align=False, stepwise_masking=False, **kwargs):
         """
         Probability of mutations
         """
-        logits = self._predict_logits(seqs)
-        probs = logits.softmax(1).cpu().numpy()
+        if stepwise_masking:
+            logits = self._predict_logits_with_step_masking(seqs)
+        else:
+            logits = self._predict_logits(seqs)
+        probs = logits.softmax(-1).cpu().numpy()
         
         if align: return probs
             
         else: return [res_to_list(state, seq) for state, seq in zip(probs, seqs)]
-            
+        